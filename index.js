52e601fd11d83e175cef19cedbe77f5bb464f232
jovan georgievski <werbrokers@gmail.com>
2017-09-06 15:34:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,10 +17,7 @@ if(string.toUpperCase()===string)
 return 'YES INDEED!'
 if(string.toLowerCase()===string)
 return 'I can\'t hear you!'
-
-else {
-
-
+if(string()==="I love you, Grandma.")
 return 'I love you, too'
-}
+
 }
