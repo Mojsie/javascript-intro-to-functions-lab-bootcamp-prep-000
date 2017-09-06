c8aa4b38d79c4df46f9edd9ba377bfb06e86bb96
jovan georgievski <werbrokers@gmail.com>
2017-09-06 15:39:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function logWhisper(string) {
 console.log('hello')
 }
 function sayHiToGrandma(string) {
-
+console.log('I love you, Grandma.')
 if(string.toUpperCase()===string)
 return 'YES INDEED!'
 if(string.toLowerCase()===string)
 return 'I can\'t hear you!'
-console.log('I love you, Grandma.')
+
 return 'I love you, too'
 
 }
