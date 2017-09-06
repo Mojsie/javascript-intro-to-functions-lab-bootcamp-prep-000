cc0b1c29178a2cd203d7c977a9f0b1d0fb8546ea
jovan georgievski <werbrokers@gmail.com>
2017-09-06 15:19:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,6 @@ console.log('hello')
 function sayHiToGrandma(string) {
 if(string.toUpperCase()===string)
 return 'YES INDEED!'
-
+if(string.toLowerCase()===string)
+return 'I can\'t hear you!'
 }
