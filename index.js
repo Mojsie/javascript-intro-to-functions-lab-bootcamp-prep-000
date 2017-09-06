42ce6f925db5a1d31dcea774b2137d214fe23d3d
jovan georgievski <werbrokers@gmail.com>
2017-09-06 14:25:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string) {
 console.log('hello')
 }
 function sayHiToGrandma(string) {
-sayHiToGrandma("I love you, Grandma.")
-
+sayHiToGrandma('hello')
+console.log ("I can\'t hear you!")
 }
